01b642031b4ea5549cbe1b0bca7416af5fed630b
destanton <delisestanton@gmail.com>
2016-09-20 20:45:48 -0400

    added player moves to game

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -21,25 +21,31 @@
 #          ["_", "_", "_"]]
 
 # used idea for board from dictionary section of book
-board = {'0': ' ', '3': ' ', '6': ' ',
-         '1': ' ', '4': ' ', '7': ' ',
-         '2': ' ', '5': ' ', '8': ' '}
+board = {'0': ' ', '1': ' ', '2': ' ',
+         '3': ' ', '4': ' ', '5': ' ',
+         '6': ' ', '5': ' ', '8': ' '}
+
+print("Let's play Tic Tac Toe")
+print("\n")
 
 
 def board_print(board):
-    print('0' + board['0'] + '|' + '3' + board['3'] + '|' + '6' + board['6'])
+    print('0' + board['0'] + '|' + '1' + board['1'] + '|' + '2' + board['2'])
     print('--+--+--')
-    print('1' + board['1'] + '|' + '4' + board['4'] + '|' + '7' + board['7'])
+    print('3' + board['3'] + '|' + '4' + board['4'] + '|' + '5' + board['5'])
     print('--+--+--')
-    print('2' + board['2'] + '|' + '5' + board['5'] + '|' + '8' + board['8'])
+    print('6' + board['6'] + '|' + '5' + board['5'] + '|' + '8' + board['8'])
     return(board)
 board_print(board)
-
-print("Let's play Tic Tac Toe")
 
 
 while True:
     player_1 = "X"
-    turn = input("where do you want to move?")
+    print("\n")
+    turn = input("{} where do you want to move? ".format(player_1))
     board[turn] = player_1
+    board_print(board)
+    player_2 = "O"
+    turn = input("{} where do you want to move? ".format(player_2))
+    board[turn] = player_2
     board_print(board)
