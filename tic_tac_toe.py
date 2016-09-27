e24f895cf954a1e4ca861fec2547cee042142b11
destanton <delisestanton@gmail.com>
2016-09-27 14:04:16 -0400

    joel refactor

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -8,7 +8,7 @@ def clear():
     else:
         os.system('clear')
 
-board_game = [[" ", " ", " "],
+board_game_global = [[" ", " ", " "],
               [" ", " ", " "],
               [" ", " ", " "]]
 
@@ -23,67 +23,50 @@ def board_print(board):
     print("2  " + board[2][0] + "|" + board[2][1] + " |" + board[2][2])
 
 
-player_1 = "X"
-
-
 def turns(player_1, board_game):
-    clear()
-    board_print(board_game)
-    print("\n")
-    row, column = input("where do you want to move? \n*enter input as NUMBER space NUMBER: ").split(" ")
-    print("\n")
+    row, column = input("\nwhere do you want to move? \n*enter input as NUMBER space NUMBER: \n").split(" ")
     row, column = int(row), int(column)
     if board_game[row][column] == "X" or board_game[row][column] == "O":
         print("That spot's already taken")
         row, column = input("Guess again! ").split(" ")
-        # print(row, column)
         row, column = int(row), int(column)
-        board_game[row][column] = player_1
-    else:
-        board_game[row][column] = player_1  # MAGIC
-        win_condition(player_1, turns)
-    while True:
-        if player_1 == "X":
-            player_1 = "O"
-
-        else:
-            player_1 = "X"
-        turns(player_1, board_game)
+    board_game[row][column] = player_1  # MAGIC
+    return win_condition(player_1, board_game)
 
 
-def win_condition(player_1, turns):
+def win_condition(player_1, board_game):
     winner = [player_1, player_1, player_1]
     column_0 = [row[0] for row in board_game]
     column_1 = [row[1] for row in board_game]
     column_2 = [row[2] for row in board_game]
-    # print(winner)
-    # while True:
-    if board_game[0] == winner or board_game[1] == winner or board_game[2] == winner:
+    if board_game[0] == winner or board_game[1] == winner or board_game[2] == winner or column_0 == winner or column_1 == winner or column_2 == winner:
         print("You win! {}".format(player_1))
-        replay()
-    elif column_0 == winner or column_1 == winner or column_2 == winner:
-        print("You win! {}".format(player_1))
-        replay()
+        return replay(board_game)
     else:
-        pass
+        return board_game
 
 
-def replay():
+def replay(board_game):
     play_again = input("would you like to play again? Y/n ").lower()
     if play_again != "n":
-        clear()
-
+        return clear_board(board_game)
     else:
         print("Bye!")
         sys.exit()
 
 
-def clear_board():
+def clear_board(board_game):
     board_game = [[" ", " ", " "],
                   [" ", " ", " "],
                   [" ", " ", " "]]
+    return board_game
 
-    board_print(board_game)
-    # board_print(board_game)
-clear()
-turns(player_1, board_game)
+
+player_1 = "X"
+while True:
+    board_print(board_game_global)
+    board_game_global = turns(player_1, board_game_global)
+    if player_1 == "X":
+        player_1 = "O"
+    else:
+        player_1 = "X"
